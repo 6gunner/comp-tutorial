1bf48ebedb849d1ba4dfb6f1f3a7bcce2926105c
coda <coda@polyhedra.network>
2024-03-21 22:19:37 +0800

    refactor: 💡 删除不需要的产物

--- types/input/input.d.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from "react";
-declare class Input extends Component<any, any> {
-    props: any;
-    constructor(props: any);
-    handleChange: (e: any) => void;
-    render(): any;
-}
-export default Input;
